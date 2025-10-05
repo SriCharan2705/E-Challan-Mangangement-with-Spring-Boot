2aa854193e3323cd6a55a7b00238376dc4ab9a48
SriCharan2705 <sricharanchaparthy@gmail.com>
2025-10-05 09:48:44 +0530

    init commit

---
+++ src/main/java/com/example/E_Challan_MS/EChallanMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_Challan_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EChallanMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EChallanMsApplication.class, args);
+	}
+
+}
